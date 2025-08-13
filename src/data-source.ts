c5233f70f44f32df3e6b1085e7e2678d3ef4b9f9
Tony Solomon <solomontony1425@gmail.com>
2025-08-13 05:53:57 +0000

    add the data-source for importing the entities and configure the oracle

---
+++ src/data-source.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import { User } from './entities/User';
+import { Role } from './entities/Role';
+import { LoanProvider } from './entities/LoanProvider';
+import { LoanProduct } from './entities/LoanProduct';
+import { LoanDetails } from './entities/LoanDetails';
+import { Payment } from './entities/Payment';
+import { ScoringParameter } from './entities/ScoringParameter';
+import { ScoringParameterRule } from './entities/ScoringParameterRule';
+import { ScoringConfigurationHistory } from './entities/ScoringConfigurationHistory';
+
+export const AppDataSource = new DataSource({
+  type: 'oracle',
+  connectString: process.env.ORACLE_DB_CONNECT_STRING,
+  username: process.env.ORACLE_DB_USER,
+  password: process.env.ORACLE_DB_PASSWORD,
+  synchronize: false, // Set to false in production
+  logging: process.env.NODE_ENV === 'development',
+  entities: [
+    User,
+    Role,
+    LoanProvider,
+    LoanProduct,
+    LoanDetails,
+    Payment,
+    ScoringParameter,
+    ScoringParameterRule,
+    ScoringConfigurationHistory
+  ],
+  migrations: [],
+  subscribers: [],
+});
