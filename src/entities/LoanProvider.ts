34077632e7304b0fe8735b14b9771e52efa979a5
Tony Solomon <solomontony1425@gmail.com>
2025-08-15 09:22:52 +0000

    npm run seed
    > nextn@0.1.0 seed
    > ts-node -P tsconfig.seed.json -r tsco

--- src/entities/LoanProvider.ts
+++ src/entities/LoanProvider.ts
@@ -33,21 +33,21 @@ export class LoanProvider {
   @Column({ type: 'number', name: 'display_order', default: 0 })
   displayOrder!: number;
 
-  @OneToMany('User', (user) => user.provider)
+  @OneToMany('User', (user: User) => user.provider)
   users!: User[];
 
-  @OneToMany('LoanProduct', (product) => product.provider)
+  @OneToMany('LoanProduct', (product: LoanProduct) => product.provider)
   products!: LoanProduct[];
 
-  @OneToMany('LoanDetails', (loan) => loan.provider)
+  @OneToMany('LoanDetails', (loan: LoanDetails) => loan.provider)
   loans!: LoanDetails[];
 
-  @OneToMany('ScoringParameter', (parameter) => parameter.provider)
+  @OneToMany('ScoringParameter', (parameter: ScoringParameter) => parameter.provider)
   scoringParameters!: ScoringParameter[];
 
   @OneToMany(
     'ScoringConfigurationHistory',
-    (history) => history.provider
+    (history: ScoringConfigurationHistory) => history.provider
   )
   scoringConfigurationHistory!: ScoringConfigurationHistory[];
 
