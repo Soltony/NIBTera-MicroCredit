1e3f05493ed28dc31ba5bae0f54396825f1ca53f
Tony Solomon <solomontony1425@gmail.com>
2025-08-15 08:57:24 +0000

    npm run seed
    > nextn@0.1.0 seed
    > ts-node -P tsconfig.seed.json -r tsco

--- src/seeds/main.seeder.ts
+++ src/seeds/main.seeder.ts
@@ -25,11 +25,11 @@ class MainSeeder {
       console.log('Dropping existing tables...');
       const tableDropOrder = [
         'payments',
-        '_scoring_config_history_to_products', // Join table must be dropped first
-        'scoring_configuration_history',
-        'loan_details',
+        '_scoring_config_history_to_products',
         'scoring_parameter_rules',
+        'loan_details',
         'scoring_parameters',
+        'scoring_configuration_history',
         'users',
         'loan_products',
         'customers',
