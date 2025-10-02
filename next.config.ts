e5b2dd2b038d0e822aa5d5052bfc44c422370279
Tony Solomon <Tony.Solomon@nibbank.com.et>
2025-10-02 09:43:00 +0300

    sec

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,19 @@ import type {NextConfig} from 'next';
 
 const contentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' ;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   font-src 'self' https://fonts.gstatic.com;
   img-src 'self' data: https://placehold.co https://play-lh.googleusercontent.com https://github.com;
   connect-src 'self';
   frame-ancestors 'self';
+  frame-src 'self';
+  media-src 'self';
   object-src 'none';
+  base-uri 'self';
+  form-action 'self';
 `.replace(/\s{2,}/g, ' ').trim();
+
 
 
 const nextConfig: NextConfig = {
