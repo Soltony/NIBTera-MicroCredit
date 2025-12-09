be407048ec9e161d163f17ac8d3e8a9761415a78
Tony Solomon <Tony.Solomon@nibbank.com.et>
2025-12-09 15:26:24 +0300

    cookie2

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
       { protocol: 'https', hostname: 'play-lh.googleusercontent.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'github.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/**' },
+      // Removed GitHub hosts to avoid direct browser requests to GitHub
     ],
   },
 
